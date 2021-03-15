e656223e81b262f5c09be1f6cd9ec781ee1b58b0
Aaliyah <74381305+Aaliyahmp@users.noreply.github.com>
2021-03-15 08:58:01 -0500

    Add files via upload

---
+++ ShreveportCapstone/src/main/java/com/tts/ShreveportCapstone/ShreveportCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.tts.ShreveportCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShreveportCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShreveportCapstoneApplication.class, args);
+	}
+
+}
---
+++ ShreveportCapstone/src/test/java/com/tts/ShreveportCapstone/ShreveportCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.ShreveportCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShreveportCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
